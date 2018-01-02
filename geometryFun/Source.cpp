8acc27c5030f3e7fa5185e7c0f1aaf3138bed97b
JonathanWoollett-Light <jonathanwoollettlight@gmail.com>
2018-01-02 23:41:43 +0000

    A few things (this is a personnal project don't judge me)

--- geometryFun/Source.cpp
+++ geometryFun/Source.cpp
@@ -6,11 +6,13 @@
 
 double const r = 1;
 
+shape sphereApproximation;
+
 struct vertice {
 	int accuracy;
 	double x, y, z;
-	std::vector<vertice*> linkedVertices;
-	vertice(double x, double y, double z, std::vector<vertice*> linkedVertices) {
+	std::vector<int> linkedVertices;
+	vertice(double x, double y, double z, std::vector<int> linkedVertices) {
 		this->x = x;
 		this->y = y;
 		this->z = z;
@@ -31,7 +33,7 @@ struct vertice {
 		std::cout << "\n--------Linked vertices--------" << std::endl;
 		std::cout << "(" << x << "," << y << "," << z << ") links to: " << std::endl;
 		for (int i = 0; i < linkedVertices.size(); i++) {
-			std::cout << "(" << linkedVertices[i]->x << "," << linkedVertices[i]->y << "," << linkedVertices[i]->z << ")" << std::endl;
+			std::cout << "(" << sphereApproximation.vertices[linkedVertices[i]].x << "," << sphereApproximation.vertices[linkedVertices[i]].y << "," << sphereApproximation.vertices[linkedVertices[i]].z << ")" << std::endl;
 		}
 		std::cout << "-------------------------------\n" << std::endl;
 	}
@@ -53,11 +55,11 @@ struct shape {
 	}
 };
 
-void octahedronBase(int accuracy = 0);
+void octahedronBase(const int accuracy = 0);
 
-vertice newVertice(vertice * a, vertice * b, int accuracy);
-bool sameOctant(vertice * a, vertice * b);
-bool distance(vertice * origin, vertice * destination);
+vertice newVertice(const vertice * a, vertice * b, const int accuracy);
+bool sameOctant(const vertice * a, const vertice * b);
+bool distance(const vertice * origin, const int destination);
 
 int main() {
 	octahedronBase(2);
@@ -65,10 +67,10 @@ int main() {
 	return 0;
 }
 
-void octahedronBase(int accuracy) {
+void octahedronBase(const int accuracy) {
 	static const vertice holder[6] = { vertice(0,r,0,0), vertice(r,0,0,0), vertice(0,0,r,0), vertice(0,-r,0,0), vertice(-r,0,0,0), vertice(0,0,-r,0) };
 	std::vector<vertice> vertices (holder, holder + (sizeof(holder) / sizeof(vertice)));
-	shape sphereApproximation = shape(0, vertices);
+	sphereApproximation = shape(0, vertices);
 	int count;
 	for (int i = 0; i < 6; i++) {
 		count = i;
@@ -79,18 +81,15 @@ void octahedronBase(int accuracy) {
 			else {
 				count++;
 			}
-			if (t == 2) {
-				continue;
+			if (t != 2) {
+				sphereApproximation.vertices[i].linkedVertices.push_back(count);
 			}
-			sphereApproximation.vertices[i].linkedVertices.push_back(&sphereApproximation.vertices[count]);
 		}
-		sphereApproximation.vertices[i].printLinks();
 	}
-	sphereApproximation.printVertices();
 
 	for (int accuracyIterator = 0; accuracyIterator < accuracy; accuracyIterator++){
 		bool * newConnection = new bool[pow(sphereApproximation.vertices.size(), 2) - sphereApproximation.vertices.size()]();
-		vertice * pointerHolder;
+		int pointerHolder;
 		for (int i = 0; i < sphereApproximation.vertices.size(); i++) {
 			std::cout << "i: " << i << std::endl;
 			for (int t = 0; t < sphereApproximation.vertices[i].linkedVertices.size(); t++) {
@@ -98,45 +97,47 @@ void octahedronBase(int accuracy) {
 				if (!newConnection[(i * (sphereApproximation.vertices.size() - 1)) + t]) {
 
 					pointerHolder = sphereApproximation.vertices[i].linkedVertices[t];
-					sphereApproximation.vertices.push_back(newVertice(&sphereApproximation.vertices[i], pointerHolder, accuracyIterator + 1));
+					sphereApproximation.vertices.push_back(newVertice(&sphereApproximation.vertices[i], &sphereApproximation.vertices[pointerHolder], accuracyIterator + 1));
 					
-					for (int q = 0; q < pointerHolder->linkedVertices.size(); q++) {
-						if (pointerHolder->linkedVertices[q] == &sphereApproximation.vertices[i]) {
-							pointerHolder->linkedVertices.erase(pointerHolder->linkedVertices.begin() + q);
+					for (int q = 0; q < sphereApproximation.vertices[pointerHolder].linkedVertices.size(); q++) {
+						if (sphereApproximation.vertices[pointerHolder].linkedVertices[q] == i) {
+							sphereApproximation.vertices[pointerHolder].linkedVertices.erase(sphereApproximation.vertices[pointerHolder].linkedVertices.begin() + q);
 							break;
 						}
 					}
 					
 					sphereApproximation.vertices[i].linkedVertices.erase(sphereApproximation.vertices[i].linkedVertices.begin() + t);
 
-					sphereApproximation.vertices[i].linkedVertices.push_back(&sphereApproximation.vertices.back());
+					sphereApproximation.vertices[i].linkedVertices.push_back(sphereApproximation.vertices.size() - 1);
 
 					std::cout << "gets here" << std::endl;
-					pointerHolder->linkedVertices.push_back(&sphereApproximation.vertices.back());
+					sphereApproximation.vertices[pointerHolder].linkedVertices.push_back(sphereApproximation.vertices.size() - 1);
 					std::cout << "does not get here" << std::endl;
 
-					sphereApproximation.vertices.back().linkedVertices.push_back(&sphereApproximation.vertices[i]);
+					sphereApproximation.vertices.back().linkedVertices.push_back(i);
 					sphereApproximation.vertices.back().linkedVertices.push_back(pointerHolder);
 				}
 			}
 		}
 		for (int q = 0; q < sphereApproximation.vertices.size(); q++) {
 			for (int p = 0; p < sphereApproximation.vertices.size() - q; p++) {
-				if (sphereApproximation.vertices[p].accuracy = (accuracyIterator + 1) && distance(&sphereApproximation.vertices[q], &sphereApproximation.vertices[p]) && sameOctant(&sphereApproximation.vertices[q], &sphereApproximation.vertices[p])) {
-					sphereApproximation.vertices[p].linkedVertices.push_back(&sphereApproximation.vertices[q]);
-					sphereApproximation.vertices[q].linkedVertices.push_back(&sphereApproximation.vertices[p]);
+				if (sphereApproximation.vertices[p].accuracy = (accuracyIterator + 1) && 
+					distance(&sphereApproximation.vertices[q], p) &&
+					sameOctant(&sphereApproximation.vertices[q], &sphereApproximation.vertices[p]))
+				{
+					sphereApproximation.vertices[q].linkedVertices.push_back(p);
+					sphereApproximation.vertices[p].linkedVertices.push_back(q);
 				}
 			}
 		}
 	}
-
 	sphereApproximation.printVertices();
 }
 
-bool distance(vertice * origin, vertice * destination) {
+bool distance(const vertice * origin, const int destination) {
 	for (int i = 0; i < origin->linkedVertices.size(); i++) {
-		for (int t = 0; t < origin->linkedVertices[i]->linkedVertices.size(); t++) {
-			if (origin->linkedVertices[i]->linkedVertices[t] == destination) {
+		for (int t = 0; t < sphereApproximation.vertices[origin->linkedVertices[i]].linkedVertices.size(); t++) {
+			if (sphereApproximation.vertices[origin->linkedVertices[i]].linkedVertices[t] == destination) {
 				return true;
 			}
 		}
@@ -144,17 +145,18 @@ bool distance(vertice * origin, vertice * destination) {
 	return false;
 }
 
-bool sameOctant(vertice * a, vertice * b) {
+bool sameOctant(const vertice * a, const vertice * b) {
 	if (
 		((a->x >= 0 && b->x >= 0) || (a->x <= 0 && b->x <= 0)) && 
 		((a->y >= 0 && b->y >= 0) || (a->y <= 0 && b->y <= 0)) && 
-		((a->z >= 0 && b->z >= 0) || (a->z <= 0 && b->z <= 0))) {
+		((a->z >= 0 && b->z >= 0) || (a->z <= 0 && b->z <= 0))) 
+	{
 		return true;
 	}
 	return false;
 }
 
-vertice newVertice(vertice * a, vertice * b, int accuracy) {
+vertice newVertice(const vertice * a, const vertice * b, const int accuracy) {
 	vertice midpoint = vertice((b->x + a->x) / 2, (b->y + a->y) / 2, (b->z + a->z) / 2);
 
 	double midpointMagnitude = sqrt(pow(midpoint.x, 2) + pow(midpoint.y, 2) + pow(midpoint.z, 2));
